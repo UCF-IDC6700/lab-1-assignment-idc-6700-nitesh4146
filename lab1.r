8b003411ac35c31b6d7a84d784491d74d424f4f4
nitesh4146 <nitesh4146@gmail.com>
2022-01-23 21:25:16 -0500

    completed

--- lab1.r
+++ lab1.r
@@ -14,6 +14,9 @@
 #
 
 #  vvvvv Put your code down here vvvvvv
+
+# Part 1: Added to readme file
+
 # Part 2:
 lab1_data <- read.table("./lab1.dat", header=TRUE, sep=",", comment.char="")
 
@@ -58,7 +61,27 @@ print(sum(hue_into_colorintesity))
 # Part 9:
 print(runif(10, min = 0.0, max = 10.0))
 print(rnorm(10, mean = 1.0, sd = 0.3))
-
+print(dnorm(0.12))
+print(pnorm(1.96))
 
 # Part 10:
+monte_carlo_sim <- rpois(10, 2)
+print(monte_carlo_sim)
+print(cumsum(monte_carlo_sim))
 
+estimate <- function(n) {
+  return (4*sum((runif(n)^2 + runif(n)^2) < 1)/n)
+}
+
+print(estimate(10))
+print(estimate(10000))
+print(estimate(10000000))
+
+# This Monte Carlo experiment is to find the value of pi by using 
+# equation of a circle to condition two independent uniform random 
+# variables (x,y).
+# The equation (runif(n)^2 + runif(n)^2) = 1 is that of a unit circle
+# centered at origin. The condition (< 1) samples the probability that 
+# the point (x,y) lies within the quarter circle 
+# (or a circle due to multiplication by 4). This should naturally result 
+# in area of a circle (pi*r^2 = pi ...since r=1)
